bbd91c985e77e686f46b1d4ca390b66cd0b75822
Hiep <hoanghiep8401@gmail.com>
2024-11-26 01:42:34 +0700

    login sign up verify code flow

--- API/Program.cs
+++ API/Program.cs
@@ -1,34 +1,89 @@
+using API.Middleware;
+using Application;
+using Application.Interface;
+using Application.MyMapper;
+using Application.Repository;
+using Application.Services;
+using Application.Validation;
+using Domain;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Infrastructure;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = builder.Configuration.Get<AppSetting>();
+builder.Services.AddControllers();
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+builder.Services.AddFluentValidationAutoValidation();
+
+
+
+//config api 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(configuration!.ConnectionStrings.DefaultConnection);
+    //options.UseNpgsql(configuration!.ConnectionStrings.LocalDockerConnection);
+    options.ConfigureWarnings(warnings =>
+            warnings.Ignore(CoreEventId.NavigationBaseIncludeIgnored));
+});
+
+builder.Services.AddSwaggerGen
+    (
+    opt =>
+    {
+        opt.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+        {
+            Description = "Standard Authorization (\"bearer {token}\" ) ",
+            In = ParameterLocation.Header,
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey
+        });
+        opt.OperationFilter<SecurityRequirementsOperationFilter>();
+
+    }
+
+    );
+builder.Services.AddAutoMapper(typeof(MapperConfigurationsProfile).Assembly);
+builder.Services.AddSingleton(configuration!);
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IClaimService, ClaimService>();
+
+builder.Services.AddFluentValidationAutoValidation()
+    .AddValidatorsFromAssemblyContaining<RegisterValidator>();
+
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
 
+app.UseCors(p => p.SetIsOriginAllowed(origin => true).AllowAnyHeader().AllowAnyMethod().AllowCredentials());
+app.UseMiddleware<ValidationMiddleware>();
+app.UseMiddleware<ExceptionMiddleware>();
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.MapControllers();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
---
+++ Application/Services/ClaimService.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interface;
+using Domain.Entity;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Services
+{
+    public class ClaimService : IClaimService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ClaimService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public ClaimDTO GetUserClaim()
+        {
+            var tokenUserId = _httpContextAccessor.HttpContext!.User.FindFirst("UserId");
+            var tokenUserRole = _httpContextAccessor.HttpContext!.User.FindFirst("Role");
+            if (tokenUserId == null)
+            {
+                throw new ArgumentNullException("UserId can not be found!");
+            }
+            var userId = Int32.Parse(tokenUserId?.Value.ToString()!);
+            Role userRole = Enum.Parse<Role>(tokenUserRole?.Value.ToString()!);
+            var userClaim = new ClaimDTO
+            {
+                Role = userRole,
+                Id = userId
+            };
+
+            return userClaim;
+        }      
+       
+    }
+    public class ClaimDTO
+    {
+        public int Id { get; set; }
+        public Role Role { get; set; }
+    }
+
+
+}
